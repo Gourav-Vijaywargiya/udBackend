f1eaeb32064f97111a5629e27c97bd6c1f6af938
Gourav Vijaywargiya <gouravvijayvergiya44@gmail.com>
2023-02-28 15:12:30 +0530

    change upload folder path

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var cors = require('cors');
 app.use(express.json());
 
 const path = require('path');
-app.use("/uploads", express.static("/home/bs/TypeScript/myApplication/Backend/uploads/"));
+app.use("/uploads", express.static("/home/bs/udBackend/uploads"));
 
 connectToMongo();
 
